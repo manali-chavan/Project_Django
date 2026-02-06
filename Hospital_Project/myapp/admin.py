5acb3b676e3058db89f939cb1fe5b09911ef828d
zatingk18 <zatingkarbhari8289@gmail.com>
2026-02-06 13:40:29 +0530

    models page

--- Hospital_Project/myapp/admin.py
+++ Hospital_Project/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from myapp.models import *
 
 # Register your models here.
+admin.site.register(Doctor)
+
+admin.site.register(Patient)
+
+admin.site.register(Appointment)
--- Hospital_Project/myapp/models.py
+++ Hospital_Project/myapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('Pending','Pending'),
+            ('Approved','Approved'),
+            ('Cancelled', 'Cancelled')
+        ],
+        default='Pending'
+    )
+
+
+    def __str__(self):
+        return f"{self.patient} - {self.doctor}"
